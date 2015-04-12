6740b747a91530635d0d6ac9759aad1c20182ce1
moritzf <moritz.finsterwalder@gmail.com>
2015-04-12 17:06:58 +0200

    added files

--- system/env.zsh
+++ system/env.zsh
@@ -1,5 +1,6 @@
 export EDITOR='vim'
-export PATH=$PATH:/usr/sbin:/usr/bin:/usr/local/bin:/user/local/sbin:/bin/:/usr/local/texlive/2013/bin/x86_64-darwin:/usr/local/texlive/2013/bin/universal-darwin
+export PATH="$PATH:/usr/sbin:/usr/bin:/usr/local/bin:/user/local/sbin:/bin/:/usr/local/texlive/2013/bin/x86_64-darwin:/usr/local/texlive/2013/bin/universal-darwin:/Applications/Android Studio.app/sdk/tools:/Applications/Android Studio.app/sdk/platform-tools"
 
+export ANDROID_HOME="/Applications/Android Studio.app/sdk"
 export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python2.7/site-packages
 
